b521e0eae0aa8898851a73454a42b7c1ba57b616
unknown <soheilhejazi1@gmail.com>
2024-08-10 18:51:04 +0430

    add meta box

--- installment.php
+++ installment.php
@@ -24,3 +24,39 @@ add_action('admin_menu', function () {
             echo "<br><br><input style='display: flex;' type='text'><input type='submit' value='افزودن'>";
         });
 });
+add_shortcode('contact-form',function (){
+    if (isset($_post['sub'])){
+        echo "salam";
+    }
+
+
+    ?>
+    <form>
+        <label>نام:</label><input name="fname" type="text"><br>
+        <label>فامیلی:</label><input name="lname" type="text"><br>
+        <button name="sub">ثبت</button>
+    </form>
+
+
+
+    <?php
+
+
+
+});?>
+<?php
+add_action('add_meta_boxes','boxs');
+function boxs()
+{
+    add_meta_box('addbox','اطلاعات اضافی نوشته',function (){
+        ?>
+        <label>نام نویسنده</label>
+        <input type="text" id="textbox">
+        <label>آدرس</label>
+        <input type="url" id="urlbox">
+        <?php
+    },'post','advanced','high');
+}
+
+
+?>
